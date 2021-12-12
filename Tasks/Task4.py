7d4b5ceb01ab2cf6457e2b7298e73b6614849c34
asifrahman15 <asifrahman15@gmail.com>
2021-12-12 14:11:58 +0530

    12 December 2021

---
+++ Tasks/Task4.py
@@ -0,0 +1,7 @@
+from pymongo import MongoClient
+
+client = MongoClient(f'''mongodb+srv://Rahman:rahman12345@rahmanmaroonintern.lmf10.mongodb.net/myFirstDatabase?retryWrites=true&w=majority''')
+taskSlack = client.taskSlack
+collection =  = taskSlack.PrasanthMessages
+
+
